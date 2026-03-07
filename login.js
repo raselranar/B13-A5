9bd349aad4075e3f214bb6a9d26a1eb1f3dc07f8
Rasel <raselranar2005@gmail.com>
2026-03-07 21:07:57 +0600

    add login functionality

---
+++ login.js
@@ -0,0 +1,15 @@
+const userNameInput = document.getElementById("username");
+const passwordInput = document.getElementById("password");
+const loginButton = document.getElementById("login-btn");
+
+loginButton.addEventListener("click", (e) => {
+  const userName = userNameInput.value.trim();
+  const password = passwordInput.value.trim();
+
+  if (userName === "admin" && password === "admin123") {
+    console.log("login successful");
+    window.location.href = "/home.html";
+    return;
+  }
+  alert("Please enter correct Username and password");
+});
